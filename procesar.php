56f594e6e054cffd2a52aa1975707893a42d6af0
Hugo1209 <70726715+Hugo1209@users.noreply.github.com>
2020-11-26 04:41:52 -0600

    Configurar Validacion
    nombre, telefono, correo, puesto

---
+++ procesar.php
@@ -0,0 +1,13 @@
+<?php
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
+$puesto = $_POST['puesto'];
+
+
+echo "Recibimos... <br>";
+echo "Nombre: ".$nombre."<br>";
+echo "Telefono: ".$telefono."<br>";
+echo "Email: ".$email."<br>";
+echo "Puesto: ".$puesto."<br>";
+?>
